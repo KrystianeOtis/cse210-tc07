3c5f29cc68c77f31a1a619af040f279f28973687
KrystianeOtis <oti19001@byui.edu>
2021-06-05 13:16:50 -0600

    edited word file

--- speed_template/speed/game/director.py
+++ speed_template/speed/game/director.py
@@ -1,18 +1,18 @@
 from time import sleep
 from game import constants
-from game.food import Food
+from game.words import Words
 from game.score import Score
 from game.snake import Snake
 
 class Director:
-    """A code template for a person who directs the game. The responsibility of 
+    """A code template for a person who directs the game. The responsibility of
     this class of objects is to control the sequence of play.
-    
+
     Stereotype:
         Controller
 
     Attributes:
-        food (Food): The snake's target.
+        words (Words): targeted word for user to type
         input_service (InputService): The input mechanism.
         keep_playing (boolean): Whether or not the game can continue.
         output_service (OutputService): The output mechanism.
@@ -22,20 +22,20 @@ class Director:
 
     def __init__(self, input_service, output_service):
         """The class constructor.
-        
+
         Args:
             self (Director): an instance of Director.
         """
-        self._food = Food()
+        self._words = Words()
         self._input_service = input_service
         self._keep_playing = True
         self._output_service = output_service
         self._score = Score()
         self._snake = Snake()
-        
+
     def start_game(self):
         """Starts the game loop to control the sequence of play.
-        
+
         Args:
             self (Director): an instance of Director.
         """
@@ -56,31 +56,31 @@ class Director:
         self._snake.move_head(direction)
 
     def _do_updates(self):
-        """Updates the important game information for each round of play. In 
+        """Updates the important game information for each round of play. In
         this case, that means checking for a collision and updating the score.
 
         Args:
             self (Director): An instance of Director.
         """
         self._handle_body_collision()
-        self._handle_food_collision()
-        
+        self._handle_words_collision()
+
     def _do_outputs(self):
-        """Outputs the important game information for each round of play. In 
-        this case, that means checking if there are stones left and declaring 
+        """Outputs the important game information for each round of play. In
+        this case, that means checking if there are stones left and declaring
         the winner.
 
         Args:
             self (Director): An instance of Director.
         """
         self._output_service.clear_screen()
-        self._output_service.draw_actor(self._food)
+        self._output_service.draw_actor(self._words)
         self._output_service.draw_actors(self._snake.get_all())
         self._output_service.draw_actor(self._score)
         self._output_service.flush_buffer()
 
     def _handle_body_collision(self):
-        """Handles collisions between the snake's head and body. Stops the game 
+        """Handles collisions between the snake's head and body. Stops the game
         if there is one.
 
         Args:
@@ -93,17 +93,16 @@ class Director:
                 self._keep_playing = False
                 break
 
-    def _handle_food_collision(self):
-        """Handles collisions between the snake's head and the food. Grows the 
-        snake, updates the score and moves the food if there is one.
+    def _handle_words_collision(self):
+        """Handles collisions the bottom of the board and the words if user doesn't type fast enough.
 
         Args:
             self (Director): An instance of Director.
         """
         head = self._snake.get_head()
-        if head.get_position().equals(self._food.get_position()):
-            points = self._food.get_points()
+        if head.get_position().equals(self._words.get_position()):
+            points = self._words.get_points()
             for n in range(points):
                 self._snake.grow_tail()
             self._score.add_points(points)
-            self._food.reset() 
+            self._words.reset()
--- speed_template/speed/game/words.py
+++ speed_template/speed/game/words.py
@@ -3,23 +3,32 @@ from game import constants
 from game.actor import Actor
 from game.point import Point
 
-# TODO: Define the Food class here
-class Food(Actor):
+class Words(Actor):
     def __init__(self):
         super().__init__()
         position = Point(random.randrange(0, constants.MAX_X), random.randrange(1, constants.MAX_Y))
         self.set_position(position)
         self._points = random.randint(1,5)
         self.set_text("@")
+        self.lists = []
+
+    def get_word(self):
+        with open("speed_template\speed\game\words.txt") as words:
+            for i in words:
+                i = i.strip()
+                self.lists.append(i)
+        # print(lists)
+        n = 5
+        five = print(random.sample(self.lists, n))
         
 
     def get_points(self):
 
         return self._points
-        
+
 
     def reset(self):
-        
+
         self._points = random.randint(1,5)
         position = Point(random.randrange(1, constants.MAX_X), random.randrange(1, constants.MAX_Y))
         self.set_position(position)
